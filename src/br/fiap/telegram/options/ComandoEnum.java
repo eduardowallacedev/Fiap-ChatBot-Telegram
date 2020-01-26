361ccbb0ea7e371f261ea046f599b651a47facba
eduardowallacedev <eduardo.wallace@outlook.com.br>
2020-01-26 18:19:51 -0300

    ajuste de comandos enum

--- src/br/fiap/telegram/options/ComandoEnum.java
+++ src/br/fiap/telegram/options/ComandoEnum.java
@@ -2,14 +2,17 @@ package br.fiap.telegram.options;
 
 public enum ComandoEnum {
 
-	START("/start", "Inicio com menu de boas vindas"), ABRIR_CONTA("/abrirconta", "Abrir uma nova conta"),
-	MODIFICAR_CONTA("/modificarconta", "Atualiza informacoes da conta"),
-	EXIBIR_DADOS("/exibirdados", "Exibe os dados da sua conta"),
-	DEPOSITO("/deposito", "Deposita um valor (formato R$ 99.99)"), SAQUE("/saque", "Saca um valor (formato R$ 99.99)"),
-	EXTRATO("/extrato", "Extrato da conta"), EMPRESTIMO("/emprestimo", "Emprestimos efetuados"),
-	SALDO_DEVEDOR("/saldodevedor", "Saldo devedor de emprestimos"),
-	LANCAMENTOS("/lancamentos", "Resumos dos lanÃ§amentos"), RETIRADAS("/retiradas", "Resumo dos saques"),
-	TARIFAS("/tarifas", "Resumo de tarifas geradas"), AJUDA("/ajuda", "Ajuda");
+	 START("/start", "Inicio com menu de boas vindas")
+	,ABRIR_CONTA("/abrirconta", "Abrir uma nova conta")
+	,MODIFICAR_CONTA("/modificarconta", "Atualiza informacoes da conta")
+	,EXIBIR_DADOS("/exibirdados", "Exibe os dados da sua conta")
+	,DEPOSITO("/deposito", "Deposita um valor (formato R$ 99.99)")
+	,SAQUE("/saque", "Saca um valor (formato R$ 99.99)")
+	,EXTRATO("/extrato", "Extrato da conta")
+	,LANCAMENTOS("/lancamentos", "Resumos dos lançamentos")
+	,RETIRADAS("/retiradas", "Resumo dos saques")
+	,TARIFAS("/tarifas", "Resumo de tarifas geradas")
+	,AJUDA("/ajuda", "Ajuda");
 
 	public String codigo;
 	public String descricao;
