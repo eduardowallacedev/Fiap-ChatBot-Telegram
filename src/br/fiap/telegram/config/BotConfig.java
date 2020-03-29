5c3c0e12434d9f38cba211ae26ec4dc2afe8eb96
Eduardo Wallace <52458043+eduardowallacedev@users.noreply.github.com>
2020-03-29 18:39:46 -0300

    Update BotConfig.java
    Alterado para nome e token genérico

--- src/br/fiap/telegram/config/BotConfig.java
+++ src/br/fiap/telegram/config/BotConfig.java
@@ -3,10 +3,10 @@ package br.fiap.telegram.config;
 public class BotConfig {
 
     // Nome do Bot Utilizado  
-    public static final String BOT_NOME = "@WallaceBankBot";
+    public static final String BOT_NOME = "@@@@";
     // Informar o token gerado pelo BotFather
-    public static final String BOT_TOKEN = "984457931:AAE2PzkbTPxoxIvJYFMkmK517m5ft7YGIlg";
-    // DiretÛrio onde ficar· salvo a base de dados
+    public static final String BOT_TOKEN = "@@@@";
+    // Diret√≥rio onde ficar√° salvo a base de dados
     public static final String BOT_DB_DIRECTORY = "C:\\PrevaylerDB\\WallaceBankBot";
     
 }
